756045de00cd24acbe9205b49ce7ae290f6208b8
Hamshita Kancharlapalli <hamshita11@gmail.com>
2022-03-14 13:05:30 +0530

    cookiecutter_template

---
+++ mlflow_cookiecutter/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Cookiecutter template for complete end-to-end implementation of MLops using tools like mlflow,dvc,git.',
+    author='Hamshita',
+    license='MIT',
+)
